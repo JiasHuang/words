42b6cbd97e78befc2c27f4049f67e689132ab68a
jiashuang <jias.tw@gmail.com>
2018-03-27 09:11:43 +0800

    init -2

--- list.js
+++ list.js
@@ -1,40 +1,68 @@
 
 var highlights = [];
 var ignores = [];
+var bMenubox = 0;
+
+function toggleMenubox(word = null) {
+    let element = document.getElementById('menubox');
+    if (!bMenubox && word) {
+        text = '';
+        text += '<p>' + word + '</p>\n';
+        text += '<table>\n';
+        text += '<tr><td>Ignore</td><td>Highlight</td></tr>\n';
+        text += '<tr><td>Cambridge</td><td>Webster</td></tr>\n';
+        text += '</table>\n';
+        element.innerHTML = text;
+        element.style.display = 'block';
+        bMenubox = true;
+    } else {
+        element.style.display = 'none';
+        bMenubox = false;
+    }
+}
 
 function saveLocalStorage() {
     localStorage.setItem('highlights', highlights.join('_'));
     localStorage.setItem('ignores', ignores.join('_'));
 }
 
-function highlight(element) {
-    let word = element.dataset.word;
+function toggleHighlight(word) {
+    let element = document.querySelector('div[data-word='+word+']');
     if (ignores.indexOf(word) >= 0) {
         let index = ignores.indexOf(word);
         element.classList.remove('ignore');
-        element.getElementsByTagName('p')[0].style.display = 'block';
+        element.getElementsByTagName('p')[1].style.display = 'block';
         ignores.splice(index, 1);
-    } else {
-        if (highlights.indexOf(word) < 0)
-            highlights.push(word);
-        element.classList.add('highlight');
     }
-    saveLocalStorage();
-}
-
-function ignore(element) {
-    let word = element.dataset.word;
     if (highlights.indexOf(word) >= 0) {
         let index = highlights.indexOf(word);
         element.classList.remove('highlight');
         highlights.splice(index, 1);
-    } else {
-        if (ignores.indexOf(word) < 0)
-            ignores.push(word);
-        element.classList.add('ignore');
-        element.getElementsByTagName('p')[0].style.display = 'none';
     }
-    saveLocalStorage();
+    else {
+        highlights.push(word);
+        element.classList.add('highlight');
+    }
+}
+
+function toggleIgnore(word) {
+    let element = document.querySelector('div[data-word='+word+']');
+    if (highlights.indexOf(word) >= 0) {
+        let index = highlights.indexOf(word);
+        element.classList.remove('highlight');
+        highlights.splice(index, 1);
+    }
+    if (ignores.indexOf(word) >= 0) {
+        let index = ignores.indexOf(word);
+        element.classList.remove('ignore');
+        element.getElementsByTagName('p')[1].style.display = 'block';
+        ignores.splice(index, 1);
+    }
+    else {
+        ignores.push(word);
+        element.classList.add('ignore');
+        element.getElementsByTagName('p')[1].style.display = 'none';
+    }
 }
 
 function openTab_cambridge(word) {
@@ -46,18 +74,26 @@ function openTab_webster(word) {
 }
 
 function onClick(event) {
-    if (event.target.matches('span.float_right')) {
-        let parent = event.target.parentElement;
-        let word = parent.dataset.word;
+    let element = event.target.closest('div');
+    if (element && element.hasAttribute('data-word')) {
+        let word = element.dataset.word;
         event.preventDefault();
-        if (event.target.innerHTML === '[+]')
-            highlight(parent);
-        else if (event.target.innerHTML === '[-]')
-            ignore(parent)
-        else if (event.target.innerHTML === '[cambridge]')
+        toggleMenubox(word);
+    } else if (element.classList.contains('menubox')) {
+        let word = element.getElementsByTagName('p')[0].innerHTML;
+        if (event.target.innerHTML === 'Ignore') {
+            toggleIgnore(word);
+            saveLocalStorage();
+        } else if (event.target.innerHTML === 'Highlight') {
+            toggleHighlight(word);
+            saveLocalStorage();
+        } else if (event.target.innerHTML === 'Cambridge') {
             openTab_cambridge(word);
-        else if (event.target.innerHTML === '[webster]')
+        } else if (event.target.innerHTML === 'Webster') {
             openTab_webster(word);
+        }
+    } else {
+        toggleMenubox();
     }
 }
 
@@ -66,34 +102,32 @@ function finalize(element) {
     let type = element.dataset.type;
     let desc = element.innerHTML;
     let text = '';
-    text += '<span class="float_right">[webster]</span>\n';
-    text += '<span class="float_right">[cambridge]</span>\n';
-    text += '<span class="float_right">[+]</span>\n';
-    text += '<span class="float_right">[-]</span>\n';
-    text += '<span>' + word + ' [' + type + ']</span>\n';
+    text += '<p>' + word + ' [' + type + ']</p>\n';
     text += '<p>' + desc + '</p>\n';
     element.innerHTML = text;
-    if (highlights.indexOf(word) >= 0)
-        highlight(element);
-    if (ignores.indexOf(word) >= 0)
-        ignore(element);
 }
 
 function onDocumentReady() {
 
-    let s_highlights = localStorage.getItem('highlights');
-    if (s_highlights !== null) {
-        highlights = s_highlights.split('_');
-    }
-
-    let s_ignores = localStorage.getItem('ignores');
-    if (s_ignores !== null) {
-        ignores = s_ignores.split('_');
-    }
-
     let elements = document.querySelectorAll('[data-word]');
     for (let i=0;  i< elements.length; i++) {
         finalize(elements[i]);
+    }
+
+    let s_highlights = localStorage.getItem('highlights');
+    if (s_highlights !== null && s_highlights.length > 0) {
+        let words = s_highlights.split('_');
+        for (let i=0; i<words.length; i++) {
+            toggleHighlight(words[i]);
+        }
+    }
+
+    let s_ignores = localStorage.getItem('ignores');
+    if (s_ignores !== null && s_ignores.length > 0) {
+        let words = s_ignores.split('_');
+        for (let i=0; i<words.length; i++) {
+            toggleIgnore(words[i]);
+        }
     }
 
     document.addEventListener('click', onClick);
