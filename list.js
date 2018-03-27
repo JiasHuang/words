04e0bdfc48ace55315c7697a55668d33af67093d
jias_huang <jias_huang@realtek.com>
2018-03-27 13:09:48 +0800

    init -3

--- list.js
+++ list.js
@@ -1,18 +1,15 @@
 
 var highlights = [];
-var ignores = [];
-var bMenubox = 0;
+var bMenubox = false;
 
-function toggleMenubox(word = null) {
+function saveLocalStorage() {
+    localStorage.setItem('highlights', highlights.join('_'));
+}
+
+function toggleMenubox(word) {
     let element = document.getElementById('menubox');
     if (!bMenubox && word) {
-        text = '';
-        text += '<p>' + word + '</p>\n';
-        text += '<table>\n';
-        text += '<tr><td>Ignore</td><td>Highlight</td></tr>\n';
-        text += '<tr><td>Cambridge</td><td>Webster</td></tr>\n';
-        text += '</table>\n';
-        element.innerHTML = text;
+        element.getElementsByTagName('p')[0].innerHTML = word;
         element.style.display = 'block';
         bMenubox = true;
     } else {
@@ -21,21 +18,10 @@ function toggleMenubox(word = null) {
     }
 }
 
-function saveLocalStorage() {
-    localStorage.setItem('highlights', highlights.join('_'));
-    localStorage.setItem('ignores', ignores.join('_'));
-}
-
 function toggleHighlight(word) {
     let element = document.querySelector('div[data-word='+word+']');
-    if (ignores.indexOf(word) >= 0) {
-        let index = ignores.indexOf(word);
-        element.classList.remove('ignore');
-        element.getElementsByTagName('p')[1].style.display = 'block';
-        ignores.splice(index, 1);
-    }
-    if (highlights.indexOf(word) >= 0) {
-        let index = highlights.indexOf(word);
+    let index = highlights.indexOf(word);
+    if (index >= 0) {
         element.classList.remove('highlight');
         highlights.splice(index, 1);
     }
@@ -45,32 +31,21 @@ function toggleHighlight(word) {
     }
 }
 
-function toggleIgnore(word) {
-    let element = document.querySelector('div[data-word='+word+']');
-    if (highlights.indexOf(word) >= 0) {
-        let index = highlights.indexOf(word);
-        element.classList.remove('highlight');
-        highlights.splice(index, 1);
-    }
-    if (ignores.indexOf(word) >= 0) {
-        let index = ignores.indexOf(word);
-        element.classList.remove('ignore');
-        element.getElementsByTagName('p')[1].style.display = 'block';
-        ignores.splice(index, 1);
-    }
-    else {
-        ignores.push(word);
-        element.classList.add('ignore');
-        element.getElementsByTagName('p')[1].style.display = 'none';
+function onOpenTab(site) {
+    let element = document.getElementById('menubox');
+    let word = element.getElementsByTagName('p')[0].innerHTML;
+    if (site == 'webster') {
+        window.open('https://www.merriam-webster.com/dictionary/'+word, 'webster');
+    } else if (site == 'cambridge') {
+        window.open('https://dictionary.cambridge.org/zht/%E8%A9%9E%E5%85%B8/%E8%8B%B1%E8%AA%9E-%E6%BC%A2%E8%AA%9E-%E7%B9%81%E9%AB%94/'+word, 'cambridge');
     }
 }
 
-function openTab_cambridge(word) {
-    window.open('https://dictionary.cambridge.org/zht/%E8%A9%9E%E5%85%B8/%E8%8B%B1%E8%AA%9E-%E6%BC%A2%E8%AA%9E-%E7%B9%81%E9%AB%94/'+word, 'cambridge');
-}
-
-function openTab_webster(word) {
-    window.open('https://www.merriam-webster.com/dictionary/'+word, 'webster');
+function onToggleHighlight() {
+    let element = document.getElementById('menubox');
+    let word = element.getElementsByTagName('p')[0].innerHTML;
+    toggleHighlight(word);
+    saveLocalStorage();
 }
 
 function onClick(event) {
@@ -79,21 +54,6 @@ function onClick(event) {
         let word = element.dataset.word;
         event.preventDefault();
         toggleMenubox(word);
-    } else if (element.classList.contains('menubox')) {
-        let word = element.getElementsByTagName('p')[0].innerHTML;
-        if (event.target.innerHTML === 'Ignore') {
-            toggleIgnore(word);
-            saveLocalStorage();
-        } else if (event.target.innerHTML === 'Highlight') {
-            toggleHighlight(word);
-            saveLocalStorage();
-        } else if (event.target.innerHTML === 'Cambridge') {
-            openTab_cambridge(word);
-        } else if (event.target.innerHTML === 'Webster') {
-            openTab_webster(word);
-        }
-    } else {
-        toggleMenubox();
     }
 }
 
@@ -110,25 +70,50 @@ function finalize(element) {
 function onDocumentReady() {
 
     let elements = document.querySelectorAll('[data-word]');
-    for (let i=0;  i< elements.length; i++) {
+    for (let i=0;  i<elements.length; i++) {
         finalize(elements[i]);
     }
 
     let s_highlights = localStorage.getItem('highlights');
-    if (s_highlights !== null && s_highlights.length > 0) {
+    if (s_highlights !== null) {
         let words = s_highlights.split('_');
         for (let i=0; i<words.length; i++) {
-            toggleHighlight(words[i]);
-        }
-    }
-
-    let s_ignores = localStorage.getItem('ignores');
-    if (s_ignores !== null && s_ignores.length > 0) {
-        let words = s_ignores.split('_');
-        for (let i=0; i<words.length; i++) {
-            toggleIgnore(words[i]);
+            if (words[i].length > 0)
+                toggleHighlight(words[i]);
         }
     }
 
     document.addEventListener('click', onClick);
+    show10();
+}
+
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min) ) + min;
+}
+
+function hideAll() {
+    let elements = document.querySelectorAll('[data-word]');
+    for (let i=0;  i<elements.length; i++)
+        elements[i].style.display = 'none';
+}
+
+function showAll() {
+    let elements = document.querySelectorAll('[data-word]');
+    for (let i=0;  i<elements.length; i++)
+        elements[i].style.display = 'block';
+}
+
+function show10() {
+    hideAll();
+    let elements = document.querySelectorAll('[data-word]');
+    for (let i=0;  i<10; i++)
+        elements[getRndInteger(0, elements.length)].style.display = 'block';
+}
+
+function showHLs() {
+    hideAll();
+    let elements = document.querySelectorAll('[data-word].highlight');
+    console.log(elements.length);
+    for (let i=0;  i<elements.length; i++)
+        elements[i].style.display = 'block';
 }
