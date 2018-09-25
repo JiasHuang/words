105dfa394bf9ee7deb6ae515d7d68be57ba9d1b2
jiashuang <jias.tw@gmail.com>
2018-09-25 19:16:48 +0800

    refine db.js

--- db.js
+++ db.js
@@ -1,3 +1,32 @@
+var xDown = null;
+var yDown = null;
+var prevHL = null;
+var nextHL = null;
+
+function handleTouchStart(event)
+{
+    xDown = event.touches[0].clientX;
+    yDown = event.touches[0].clientY;
+}
+
+function handleTouchMove(event)
+{
+    if (xDown && yDown) {
+        let xUp = event.touches[0].clientX;
+        let yUp = event.touches[0].clientY;
+        let xDiff = xDown - xUp;
+        let yDiff = yDown - yUp;
+        if (Math.abs(xDiff) > 10 && Math.abs(yDiff) < 10) {
+            if (xDiff > 0) {
+                window.location.href = 'db.html?q='+nextHL;
+            } else {
+                window.location.href = 'db.html?q='+prevHL;
+            }
+        }
+        xDown = null;
+        yDown = null;
+    }
+}
 
 function GetURLParameter(sParam)
 {
@@ -42,11 +71,31 @@ function updateResult(q)
     document.getElementById("result").innerHTML = text;
 }
 
+function loadHLs(q)
+{
+    let s_highlights = localStorage.getItem('highlights');
+    console.log(s_highlights);
+    if (s_highlights !== null) {
+        let highlights = s_highlights.split('_');
+        let index = highlights.indexOf(q);
+        if (index >= 0) {
+            prevHL = highlights[(index+highlights.length-1) % highlights.length];
+            nextHL = highlights[(index+1) % highlights.length];
+        }
+    }
+}
+
 function onDocumentReady()
 {
     var q = GetURLParameter('q');
     updateAudio(q);
     updateResult(q);
     document.addEventListener('click', playAudio);
+
+    loadHLs(q);
+    if (prevHL && nextHL) {
+        document.addEventListener('touchstart', handleTouchStart, false);
+        document.addEventListener('touchmove', handleTouchMove, false);
+    }
 }
 
